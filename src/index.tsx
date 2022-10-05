502ac4fdc416e8578cf6b30a30c5068590f1d6b4
Ivan Diaz <ivandzb11@gmail.com>
2022-10-04 19:54:08 -0500

    Changes in react router

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { StyledEngineProvider } from "@mui/material";
 
 const root = ReactDOM.createRoot(
@@ -11,9 +11,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
-      <Router>
+      <HashRouter>
         <App />
-      </Router>
+      </HashRouter>
     </StyledEngineProvider>
   </React.StrictMode>
 );
